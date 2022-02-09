2bd9b81e92b8ccc803c2a146764820d105ceb603
samarthgarg0622 <samarth.garg@zopsmart.com>
2022-02-09 11:56:00 +0530

    intent applied on form.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
     String tag="MainActivity.tag";
-
+    EditText name,job;
+    EditText age;
+    Button sb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.Name);
+        age = findViewById(R.id.age);
+        job = findViewById(R.id.CompanyName);
+        sb = findViewById(R.id.SubmitButton);
+        sb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("Name", name.getText().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        String name = getIntent().getStringExtra("Name");
+        Log.d("activity 2", name);
     }
 }
